a2d3e7f73a1f0380ba7470fbf8870b755672acc4
anandmahangade <162805513+anandmahangade@users.noreply.github.com>
2024-10-17 17:31:08 +0530

    Update dbconnection.php

--- otss/includes/dbconnection.php
+++ otss/includes/dbconnection.php
@@ -1,8 +1,8 @@
 <?php 
 // DB credentials.
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','tiffin1');
+define('DB_PASS','Satara@123');
 define('DB_NAME','otssdb');
 // Establish database connection.
 try
